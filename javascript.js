6adf9e824cb5c2e821e66c83013937b11f816fb9
Vincent <Vincentnguyen030@gmail.com>
2023-05-24 18:09:00 -0500

    change background color of cell when mouse hovers

--- javascript.js
+++ javascript.js
@@ -4,7 +4,7 @@ function createGrid(num = 16) {
     for (let i = 0; i < num; i++) {
         let row = document.createElement('div');
         row.textContent = '';
-        row.style.cssText = 'flex:auto; border: solid black; margin:-1px; padding:0; display:flex';
+        row.style.cssText = 'flex:auto; margin:-1px; padding:0; display:flex';
         container.appendChild(row);
         for (let j = 0; j < num; j++) {
             if (j == num - 1) {
@@ -16,7 +16,7 @@ function createGrid(num = 16) {
             } else {
                 let column = document.createElement('div');
                 column.textContent = '';
-                column.style.cssText = 'flex:auto; border-right:solid black; margin: -1px; padding: 0';
+                column.style.cssText = 'flex:auto; margin: -1px; padding: 0';
                 column.classList.add('cell');
                 row.appendChild(column);
             }
@@ -24,7 +24,7 @@ function createGrid(num = 16) {
     }
 }
 
-container.style.cssText = 'display:flex; flex-direction:column; width: 40vw; height: 40vw; margin: 0 auto';
+container.style.cssText = 'display:flex; flex-direction:column; width: 40vw; height: 40vw; margin: 0 auto; border: solid black';
 
 createGrid();
 
