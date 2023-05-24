7f3edcba54c670450bb5f0bc3e098b2d41d237c8
Vincent <Vincentnguyen030@gmail.com>
2023-05-24 16:17:48 -0500

    fix borders on grid

--- javascript.js
+++ javascript.js
@@ -7,10 +7,17 @@ function createGrid(num = 16) {
         row.style.cssText = 'flex:auto; border: solid black; margin:-1px; padding:0; display:flex';
         container.appendChild(row);
         for (let i = 0; i < num; i++) {
-            let column = document.createElement('div');
-            column.textContent = '';
-            column.style.cssText = 'flex:auto; border:solid black; margin: -1px; padding: 0';
-            row.appendChild(column);
+            if (i == num - 1) {
+                let column = document.createElement('div');
+                column.textContent = '';
+                column.style.cssText = 'flex:auto; margin: -1px; padding: 0';
+                row.appendChild(column);
+            } else {
+                let column = document.createElement('div');
+                column.textContent = '';
+                column.style.cssText = 'flex:auto; border-right:solid black; margin: -1px; padding: 0';
+                row.appendChild(column);
+            }
         }
     }
 }
