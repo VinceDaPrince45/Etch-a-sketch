50deb7f75471ad258815bc7b549e4e304da551e9
Vincent <Vincentnguyen030@gmail.com>
2023-05-24 14:45:16 -0500

    create equally sized rows

--- javascript.js
+++ javascript.js
@@ -4,10 +4,10 @@ function createGrid(num = 16) {
     for (let i = 0; i < num; i++) {
         let row = document.createElement('div');
         row.textContent = '';
-        row.style.cssText = 'padding:5px; margin:0; border: solid black';
+        row.style.cssText = 'flex:auto; border: solid black; margin:-1px; padding:0';
         container.appendChild(row);
     }
 }
 
 createGrid();
-container.style.cssText = 'display:flex;';   
+container.style.cssText = 'display:flex; flex-direction:column; height:90vh';   
