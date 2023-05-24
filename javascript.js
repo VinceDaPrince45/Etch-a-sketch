1d6402a2b64e58c02517d4390fe33f6e9f26a918
Vincent <Vincentnguyen030@gmail.com>
2023-05-24 14:34:40 -0500

    create function that creates grid based off number given

--- javascript.js
+++ javascript.js
@@ -1,1 +1,13 @@
+const container = document.querySelector('.container');
 
+function createGrid(num = 16) {
+    for (let i = 0; i < num; i++) {
+        let row = document.createElement('div');
+        row.textContent = '';
+        row.style.cssText = 'padding:5px; margin:0; border: solid black';
+        container.appendChild(row);
+    }
+}
+
+createGrid();
+container.style.cssText = 'display:flex;';   
